b156cf7d3a8f75e9565e7f9fddaadde144bb79cb
lianos <lianoglou@dnli.com>
2019-09-11 06:19:04 -0700

    Cleans up multimap human entrez -> ensembl idsin data creation for package
    1. Multimap entrez -> ensembl where the extra hits comes from genes on
       alt chromosomes are resolved to the hit that is to the main/annotated
       aseembly
    2. Multimaps are removed when the multiplicity is to ensembl genes that
       do not have a symbol, or are for fusion/run-on transcripts
       (symboles with "-") in them,

--- inst/scripts/generate-package-data.R
+++ inst/scripts/generate-package-data.R
@@ -16,6 +16,14 @@ library(dplyr)
 library(data.table)
 devtools::load_all(".")
 
+# Parse and HCOP ===============================================================
+# HCOP is the HGNC Comparison of Orthology Predictions:
+# https://www.genenames.org/help/hcop/
+#
+# This file contains ortholog maps from human to many other species, which can
+# be queried by entreaz or ensembl id. We use this instead of biomaRt since the
+# table we parse and serialize isn't too big (~ 4mb), and we don't have to
+# bang on the biomaRt server each time the user requests an ortholog map.
 
 # hcop.txt.fn <- file.path(dat.dir, "human_all_hcop_sixteen_column.txt.gz")
 hcop.txt.fn <- file.path("~/tmp", "human_all_hcop_sixteen_column.txt.gz")
@@ -48,6 +56,17 @@ sids <- distinct(hcop.all, species_id, species_name)
 # .spcecies_tbl(hcop_only = TRUE)
 write.csv(sids, sids.fn, row.names = FALSE)
 
+# Parse and save the human reference MSigDb collection =========================
+# The data.frame of MSigDb collections from msigdbr provides gene symbols and
+# entrez identifiers. We will append the human ensembl id's to this.
+#
+# We do not yet enforce a 1:1 map here, and the serialized MSigDb catalog will
+# have duplicated entrez entries for some genesets wince one entrez id can map
+# to multiple ensembl IDs.
+#
+# When there are multiple entrez -> ensembl maps, however, we will remove
+# entries that map to alternate chromosomes.
+
 # Generate base human msigdb data.frame with both entrez and ensembl info
 hdf <- local({
   tmp <- msigdbr::msigdbr(species = "Homo sapiens")
@@ -62,7 +81,7 @@ hdf <- local({
 
 # Add ensembl identifiers ... this will create duplicate gs_name,gene rows
 # which will be filtered out in the end
-hdf.ens <- local({
+ens.xref <- local({
   mart <- biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl")
   mart.result <- biomaRt::getBM(
     attributes = c("entrezgene_id", "ensembl_gene_id", "hgnc_symbol", "chromosome_name"),
@@ -70,12 +89,63 @@ hdf.ens <- local({
     values = unique(hdf$human_entrez_id),
     mart = mart)
 
-  xx <- mart.result %>%
+  mart.result %>%
     transmute(human_entrez_id = as.character(entrezgene_id),
               human_ensembl_id = ensembl_gene_id,
-              human_ensembl_symbol = hgnc_symbol)
-  left_join(hdf, xx, by = "human_entrez_id")
+              human_ensembl_symbol = hgnc_symbol,
+              chromosome = chromosome_name)
 })
+
+# annotate alt chrommosomes
+ex <- ens.xref %>%
+  mutate(alt_chromosome = grepl("_|\\.", chromosome)) %>%
+  group_by(human_entrez_id) %>%
+  mutate(n = n(), n_std = sum(!alt_chromosome), n_alt = sum(alt_chromosome)) %>%
+  ungroup()
+
+ex <- filter(ex, n == 1 | (n > 1 & !alt_chromosome))
+oops <- setdiff(ex$human_entrez_id, ens.xref$human_entrez_id)
+stopifnot(length(oops) == 0)
+
+# now annotate multimatches where one match doens't have an ensembl symbol
+#
+# 1. when an entrez id maps to multiple ensemble's, and some of those ensembl
+#    id's do not have symbols, we'll drop the ones with out symbols
+# 2. if an entrez id only maps to ensembl id's that do not have symbols, then
+#    we keep them all.
+exs <- select(ex, human_entrez_id:chromosome) %>%
+  mutate(nosymbol = nchar(human_ensembl_symbol) == 0) %>%
+  # let's remove fusions/read through genes by categorizing them as nosymbol
+  mutate(nosymbol = nosymbol | grepl("-", human_ensembl_symbol, fixed = TRUE)) %>%
+  group_by(human_entrez_id) %>%
+  mutate(n = n(), n_symbol = sum(!nosymbol), n_nosymbol = sum(nosymbol)) %>%
+  ungroup()
+exs.keep <- with(exs, {
+  n == 1 | (n > 1 & (!nosymbol & n_symbol > 0) | (n_symbol == 0))
+})
+exs.cut <- exs[exs.keep,]
+oops <- setdiff(exs.cut$human_entrez_id, ens.xref$human_entrez_id)
+stopifnot(length(oops) == 0)
+
+hdf.ens <- hdf %>%
+  left_join(select(exs.cut, human_entrez_id:chromosome), by = "human_entrez_id")
+
+oops <- setdiff(hdf.ens$gs_name, hdf$gs_name)
+stopifnot(length(oops) == 0)
+
+# got smaller
+orig.count <- count(hdf, gs_name)
+new.count <- count(hdf.ens, gs_name)
+cmp.count <- inner_join(orig.count, new.count, by = "gs_name")
+stopifnot(nrow(cmp.count) == nrow(orig.count))
+
+smaller <- filter(cmp.count, n.x > n.y)
+stopifnot(nrow(smaller) == 0)
+bigger <- filter(cmp.count, n.x < n.y) %>% arrange(n.x)
+head(bigger)
+
+subset(hdf, gs_name == bigger$gs_name[1])
+subset(hdf.ens, gs_name == bigger$gs_name[1])
 
 saveRDS(hdf.ens, msigdb.fn)
 
